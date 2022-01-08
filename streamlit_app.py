94413c1b655cb52da78427d6eea56826753720e9
nightfury1103 <61180080+nightfury1103@users.noreply.github.com>
2022-01-08 20:45:05 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,78 +4,82 @@ import streamlit as st
 from streamlit.components.v1 import iframe
 import pickle
 
-def main():
-    st.set_page_config(layout="centered", page_icon="🎓", page_title="NLP Predict")
-    st.title("🎓 Fake news prediction")
+st.set_page_config(layout="centered", page_icon="🎓", page_title="NLP Predict")
+st.title("🎓 Fake news prediction")
 
-    st.write(
-        "This app shows you how you can use the machine learning to predict fake news!"
-    )
+st.write(
+    "This app shows you how you can use the machine learning to predict fake news!"
+)
 
-    left, right = st.columns(2)
+left, right = st.columns(2)
 
-    # right.write("Here's the template we'll be using:")
+# right.write("Here's the template we'll be using:")
 
-    right.image("template.png", width=500)
+right.image("template.png", width=500)
 
-    env = Environment(loader=FileSystemLoader("."), autoescape=select_autoescape())
-    template = env.get_template("template.html")
+env = Environment(loader=FileSystemLoader("."), autoescape=select_autoescape())
+template = env.get_template("template.html")
 
 
-    left.write("Fill in the data:")
-    form = left.form("template_form")
-    student = form.text_input("Type a news")
-    course = form.selectbox(
-        "Choose model",
-        ["Multi-layer Perceptron", "Logistic classifier", "K-neighbour", "Naivebayes"],
-        index=0,
-    )
-    # grade = form.slider("Grade", 1, 100, 60)
-    submit = form.form_submit_button("Check")
+left.write("Fill in the data:")
+form = left.form("template_form")
+student = form.text_input("Type a news")
+course = form.selectbox(
+    "Choose model",
+    ["Multi-layer Perceptron", "Logistic classifier", "K-neighbour", "Naivebayes"],
+    index=0,
+)
+# grade = form.slider("Grade", 1, 100, 60)
+submit = form.form_submit_button("Check")
 
-    file = 0
-    if str(course) == 'Multi-layer Perceptron':
-        file = open("MLP.pkl",'rb')
-    if str(course) == 'Logistic classifier':
-        file = open("log_classifier.pkl",'rb')
-    if str(course) == 'K-neighbour':
-        file = open("Kneighbor_classifier.pkl",'rb')
-    if str(course) == 'Naivebayes':
-        file = open("naivebayes_classifier.pkl",'rb')
+file = 0
+model = 0
+if str(course) == 'Multi-layer Perceptron':
+    # file = open("MLP.pkl",'rb')
+    with open('MLP.pkl', 'rb') as f:
+        model = pickle.load(f)
+if str(course) == 'Logistic classifier':
+    # file = open("log_classifier.pkl",'rb')
+    with open('log_classifier.pkl', 'rb') as f:
+        model = pickle.load(f)
+if str(course) == 'K-neighbour':
+    # file = open("Kneighbor_classifier.pkl",'rb')
+    with open('Kneighbor_classifier.pkl', 'rb') as f:
+        model = pickle.load(f)
+if str(course) == 'Naivebayes':
+    # file = open("naivebayes_classifier.pkl",'rb')
+    with open('naivebayes_classifier.pkl', 'rb') as f:
+        model = pickle.load(f)
 
-    model = pickle.load(file)
+# model = pickle.load(file)
 
-    if submit:
-        with st.spinner("Predicting..."):
-            left.markdown(course)
-            if not len(student):
-                left.warning('You must type a news!')
+if submit:
+    with st.spinner("Predicting..."):
+        left.markdown(course)
+        if not len(student):
+            left.warning('You must type a news!')
+        else:
+            # model = model_dict[model_name]
+
+            # cleaned_news = text_preprocessing(news)
+
+            # text_vectorized = tfidf_vectorizer.transform([cleaned_news])
+                
+            pred = model.predict([student])[0]
+
+            if pred == 1:
+                right.success("🎉 Real news!")
             else:
-                # model = model_dict[model_name]
+                right.success("🎉 Fake news!")
 
-                # cleaned_news = text_preprocessing(news)
+            st.balloons()
 
-                # text_vectorized = tfidf_vectorizer.transform([cleaned_news])
-                    
-                pred = model.predict([student])[0]
-
-                if pred == 1:
-                    right.success("🎉 Real news!")
-                else:
-                    right.success("🎉 Fake news!")
-
-                st.balloons()
-
-                # right.success("🎉 Your diploma was generated!")
-                # st.write(html, unsafe_allow_html=True)
-                # st.write("")
-                right.download_button(
-                    "⬇️ Download models",
-                    data='abc',
-                    file_name="diploma.pdf",
-                    mime="application/octet-stream",
-                )
-
-
-if __name__ == "__main__":
-    main()
+            # right.success("🎉 Your diploma was generated!")
+            # st.write(html, unsafe_allow_html=True)
+            # st.write("")
+            right.download_button(
+                "⬇️ Download models",
+                data='abc',
+                file_name="diploma.pdf",
+                mime="application/octet-stream",
+            )
