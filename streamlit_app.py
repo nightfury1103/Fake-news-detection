c4a64277a41fa873512564391b3120b92ee32805
nightfury1103 <61180080+nightfury1103@users.noreply.github.com>
2022-01-09 11:45:32 +0700

    Final commit -- revise by branch Loc

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ from datetime import date
 import streamlit as st
 from streamlit.components.v1 import iframe
 import pickle
-import sklearn
+from preprocessing import text_preprocessing
 
 st.set_page_config(layout="centered", page_icon="🎓", page_title="NLP Predict")
 st.title("🎓 Fake news prediction")
@@ -14,50 +14,55 @@ st.write(
 
 left, right = st.columns(2)
 
-right.write("Here's the template we'll be using:")
+right.write("Your answer will show below the image!")
 
 right.image("template.png", width=500)
 
 env = Environment(loader=FileSystemLoader("."), autoescape=select_autoescape())
 template = env.get_template("template.html")
 
-
 left.write("Fill in the data:")
 form = left.form("template_form")
 student = form.text_input("Type a news")
 course = form.selectbox(
     "Choose model",
-    ["Multi-layer Perceptron", "Logistic classifier", "K-neighbour", "Naivebayes"],
+    ["Multi-layer Perceptron", "Logistic classifier", "K-neighbour", "Naivebayes", "Random Forest"],
     index=0,
 )
 # grade = form.slider("Grade", 1, 100, 60)
 submit = form.form_submit_button("Check")
 
-file = 0
+file_name = "mlp_classifier.pkl"
 if str(course) == 'Multi-layer Perceptron':
-    file = open("MLP.pkl",'rb')
+    file_name = "mlp_classifier.pkl"
 if str(course) == 'Logistic classifier':
-    file = open("log_classifier.pkl",'rb')
+    file_name = "log_classifier.pkl"
 if str(course) == 'K-neighbour':
-    file = open("Kneighbor_classifier.pkl",'rb')
+    file_name = "Kneighbor_classifier.pkl"
 if str(course) == 'Naivebayes':
-    file = open("naivebayes_classifier.pkl",'rb')
+    file_name = "naivebayes_classifier.pkl"
+if str(course) == 'Random Forest':
+    file_name = "RandomForest_classifier.pkl"
+
+file = open(file_name, 'rb')
 
 model = pickle.load(file)
 
 if submit:
     with st.spinner("Predicting..."):
-        left.markdown(course)
+        left.markdown(f"You choose {course} model!")
         if not len(student):
             left.warning('You must type a news!')
         else:
-                # model = model_dict[model_name]
+            # model = model_dict[model_name]
 
-                # cleaned_news = text_preprocessing(news)
+            # cleaned_news = text_preprocessing(news)
 
-                # text_vectorized = tfidf_vectorizer.transform([cleaned_news])
-                    
-            pred = model.predict([student])[0]
+            # text_vectorized = tfidf_vectorizer.transform([cleaned_news])
+
+            cleaned_news = text_preprocessing(student)
+
+            pred = model.predict([cleaned_news])[0]
 
             if pred == 1:
                 right.success("🎉 Real news!")
@@ -66,12 +71,12 @@ if submit:
 
             st.balloons()
 
-                # right.success("🎉 Your diploma was generated!")
-                # st.write(html, unsafe_allow_html=True)
-                # st.write("")
+            # right.success("🎉 Your diploma was generated!")
+            # st.write(html, unsafe_allow_html=True)
+            # st.write("")
             right.download_button(
                 "⬇️ Download models",
-                data='abc',
-                file_name="diploma.pdf",
+                data=file_name,
+                file_name=file_name,
                 mime="application/octet-stream",
             )
